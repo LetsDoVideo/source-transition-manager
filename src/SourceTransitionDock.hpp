a84c94a790d937875b11bcb3a1248d10a4eff457
David Maldow <David@letsdovideo.com>
2026-04-12 17:22:17 -0400

    Update SourceTransitionDock.hpp

--- src/SourceTransitionDock.hpp
+++ src/SourceTransitionDock.hpp
@@ -3,13 +3,12 @@
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QLabel>
-#include <QListWidget>
 #include <QComboBox>
 #include <QSpinBox>
 #include <QPushButton>
-#include <QScrollArea>
+#include <obs-module.h>
 #include <obs-frontend-api.h>
-#include <obs.h>
+#include <obs-scene.h>
 
 class SourceTransitionDock : public QWidget {
     Q_OBJECT
@@ -19,14 +18,13 @@ public:
     ~SourceTransitionDock();
 
 private slots:
-    void onSceneItemSelectionChanged();
+    void refreshSelectedSources();
     void onShowTransitionChanged();
     void onHideTransitionChanged();
     void onApplyToAll();
 
 private:
     void setupUI();
-    void refreshSelectedSources();
     void loadTransitionsForSource(obs_source_t *source);
     void applyTransitionToSource(obs_source_t *source,
                                   const QString &showId, int showDuration,
@@ -34,22 +32,15 @@ private:
 
     static void frontendEventCallback(obs_frontend_event event, void *data);
 
-    // UI elements
-    QVBoxLayout    *mainLayout      = nullptr;
-    QLabel         *placeholderLabel = nullptr;
-    QScrollArea    *scrollArea      = nullptr;
-    QWidget        *scrollContents  = nullptr;
-    QVBoxLayout    *scrollLayout    = nullptr;
+    QVBoxLayout  *mainLayout      = nullptr;
+    QLabel       *placeholderLabel = nullptr;
+    QWidget      *controlsWidget  = nullptr;
+    QVBoxLayout  *controlsLayout  = nullptr;
+    QComboBox    *showTransition  = nullptr;
+    QSpinBox     *showDuration    = nullptr;
+    QComboBox    *hideTransition  = nullptr;
+    QSpinBox     *hideDuration    = nullptr;
+    QPushButton  *applyAllButton  = nullptr;
 
-    // Per-source controls (shown when sources are selected)
-    QWidget        *controlsWidget  = nullptr;
-    QVBoxLayout    *controlsLayout  = nullptr;
-    QComboBox      *showTransition  = nullptr;
-    QSpinBox       *showDuration    = nullptr;
-    QComboBox      *hideTransition  = nullptr;
-    QSpinBox       *hideDuration    = nullptr;
-    QPushButton    *applyAllButton  = nullptr;
-
-    // Currently selected sources
     QList<obs_source_t *> selectedSources;
 };
